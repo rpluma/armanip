15e6a8bc41ff05a2e604747b862cb5660c542169
Ricardo <0617077537@uma.es>
2025-05-19 11:18:12 +0200

    Cambios lab2

--- advanced_robotics_ws/src/uma_arm_control/src/uma_arm_dynamics.cpp
+++ advanced_robotics_ws/src/uma_arm_control/src/uma_arm_dynamics.cpp
@@ -127,26 +127,53 @@ private:
     {
 
         // Initialize M, C, Fb, g_vec, J, and tau_ext
+        Eigen::MatrixXd M(2, 2);    // Inertia matrix
+        Eigen::VectorXd C(2);       // Coriolis and centrifugal forces matrix
+        Eigen::MatrixXd Fb(2, 2);   // Viscous friction matrix
+        Eigen::VectorXd g_vec(2);   // gravity vector
+        Eigen::MatrixXd J(2, 2);    //! jacobiano -> preguntar de donde sale
+        Eigen::VectorXd tau_ext(2); // joint torques due to external forces
 
         // Initialize q1, q2, q_dot1, and q_dot2
+        double q1 = joint_positions_(0);
+        double q2 = joint_positions_(1);
+        double q_dot1 = joint_velocities_(0);
+        double q_dot2 = joint_velocities_(1);
+
 
         // Placeholder calculations for M, C, Fb, g, and tau_ext
         // Calculate matrix M
+        M(0, 0) = m1_ * pow(l1_, 2) + m2_ * (pow(l1_, 2) + 2 * l1_ * l2_ * cos(q2) + pow(l2_, 2));
+        M(0, 1) = m2_ * (l1_ * l2_ * cos(q2) + pow(l2_, 2));
+        M(1, 0) = M(0, 1);
+        M(1, 1) = m2_ * pow(l2_, 2);
 
         // Calculate vector C (C is 2x1 because it already includes q_dot)
+        C << -m2_ * l1_ * l2_ * sin(q2) * (2 * q_dot1 * q_dot2 + pow(q_dot2, 2)),
+        m2_ * l1_ * l2_ * pow(q_dot1, 2) * sin(q2);
 
         // Calculate Fb matrix
+        Fb << b1_, 0.0,
+              0.0, b2_;
 
         // Calculate g_vect
+        g_vec << (m1_ + m2_) * l1_ * g_ * cos(q1) + m2_ * g_ * l2_ * cos(q1 + q2),
+                    m2_ * g_ * l2_ * cos(q1 + q2);
 
         // Calculate J
+        J << -l1_ * sin(q1) - l2_ * sin(q1 + q2), -l2_ * sin(q1 + q2),
+              l1_ * cos(q1) + l2_ * cos(q1 + q2),  l2_ * cos(q1 + q2);
 
         // Calculate tau_ext
+        tau_ext << J.transpose() * external_wrenches_;
 
         // Calculate joint accelerations using the dynamic model: q'' = M^(-1)[tau - C(q,q')q' - Fbq' - g(q) + tau_ext]
         Eigen::VectorXd q_ddot(2);
-        q_ddot << 0, 0;
-
+        //- Versión inicial 
+        //     q_ddot << 0, 0;
+        //- Versión 1: no se tienen en cuenta tau_ext ni Fb
+        q_ddot << M.inverse() * (joint_torques_ - C - Fb * joint_velocities_ - g_vec + tau_ext);
+        
         // Return joint accelerations
         return q_ddot;
     }
@@ -156,9 +183,11 @@ private:
     {
         // Placeholder for velocity calculation
         // Integrate velocity over the time step (elapsed_time_)
-        Eigen::VectorXd q_dot(2);
-        q_dot << 0, 0;
-
+        //- Versión inicial: valores por defeto
+        //      Eigen::VectorXd q_dot(2);
+        //      q_dot << 0, 0;
+        //- Versión 1: no se tienen en cuenta tau_ext ni Fb
+        Eigen::VectorXd q_dot = joint_velocities_ + joint_accelerations_ * elapsed_time_;
         return q_dot;
     }
 
@@ -167,9 +196,11 @@ private:
     {
         // Placeholder for position calculation
         // Integrate position over the time step (elapsed_time_)
-        Eigen::VectorXd q(2);
-        q << 0, 0;
-        
+        //- Versión inicial: valores por defeto
+        //      Eigen::VectorXd q(2);
+        //      q << 0, 0;
+        //- Versión 1: no se tienen en cuenta tau_ext ni Fb
+        Eigen::VectorXd q = joint_positions_ + joint_velocities_ * elapsed_time_;
         return q;
     }
 
